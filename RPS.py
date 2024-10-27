2d36104633d66603a171a56de6aaaa74243240b5
Ryan Dante <ryanwdante@gmail.com>
2024-10-27 14:01:40 +0900

    feat: Added prediction function for RPS.py

--- RPS.py
+++ RPS.py
@@ -1,10 +1,35 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+def player(prev_play, opponent_history=[], play_history={}):
+    if not prev_play:
+        prev_play = 'R'
 
-def player(prev_play, opponent_history=[]):
+    # Append the opponent's previous play to their history
     opponent_history.append(prev_play)
+    
+    # Default prediction
+    prediction = 'P'
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    # Update play_history with the last five moves
+    if len(opponent_history) > 4:
+        last_five = "".join(opponent_history[-5:])
+        play_history[last_five] = play_history.get(last_five, 0) + 1
+        
+        # Generate potential plays based on the last four moves and each possible next move
+        potential_plays = [
+            "".join([*opponent_history[-4:], v]) 
+            for v in ['R', 'P', 'S']
+        ]
 
-    return guess
+        # Find the most frequent move in the potential plays
+        sub_order = {
+            k: play_history[k]
+            for k in potential_plays if k in play_history
+        }
+
+        # Track the most frequent move
+        if sub_order:
+            prediction = max(sub_order, key=sub_order.get)[-1:]
+
+    # Pick the ideal response based on the most frequent move
+    ideal_response = {'P': 'S', 'R': 'P', 'S': 'R'}
+
+    return ideal_response[prediction]
